1d2778b751f6d531b26db67c23cabba1c6daae93
Kurt Higa <kurthigaa@gmail.com>
2024-02-02 23:34:20 -0600

    finish day 27

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -4,30 +4,53 @@
 //
 //  Created by Kurt Higa on 1/31/24.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
     var body: some View {
         NavigationStack{
-            VStack{
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper("\(coffeeAmount) cup(s)", value: $sleepAmount, in: 1...20)
+            Form{
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                VStack(alignment: .leading, spacing: 0){
+                    
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                VStack(alignment: .leading, spacing: 0){
+                    
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar{
                 Button  ("Calculate", action: calculateBedtime  )
+            }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK"){}
+            }message: {
+                Text(alertMessage)
             }
         }
 //        VStack{
@@ -41,16 +64,30 @@ struct ContentView: View {
 //        }
     }
     func calculateBedtime(){
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        }catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
     func exampleDates(){
-        var tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let _ = Date.now...tomorrow
     }
     func anotherExampleDates(){
         let now = Date.now
         let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = now...tomorrow
+        let _ = now...tomorrow
     }
     func exDate(){
 //        var components = DateComponents()
@@ -59,8 +96,8 @@ struct ContentView: View {
 //        let date = Calendar.current.date(from: components) ?? .now
 //
         let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+        let _ = components.hour ?? 0
+        let _ = components.minute ?? 0
     }
 }
 
