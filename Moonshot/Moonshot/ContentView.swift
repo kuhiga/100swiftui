7f4929a5d2f3680918e2e06a7638b0753143ed8f
Kurt Higa <kurthigaa@gmail.com>
2024-02-17 23:17:42 -0600

    finish day 40

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -32,33 +32,53 @@ struct ContentView: View {
     let astronauts: [String: Astronaut ] = Bundle.main.decodeToJSON("astronauts.json")
     let missions: [Mission] = Bundle.main.decodeToJSON("missions.json")
 
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
+    let columns   = [
+        GridItem(.adaptive(minimum: 150))
     ]
     var body: some View {
-        ScrollView(.horizontal){
-            LazyHGrid(rows: layout){
-                ForEach(0..<99 ){
-                     Text("Item \($0 )")
+        NavigationStack{
+            ScrollView {
+                LazyVGrid(columns: columns){
+                    ForEach(missions){ mission in
+                        NavigationLink {
+                            Text(mission.displayName)
+                                .font(.title)
+                            Text(mission.description)
+                        } label: {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                                    .padding()
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10 ))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                                )
+                        }
+                    }
                 }
+                .padding([.horizontal, .bottom])
             }
+            .navigationTitle("Moonshot")
+                  .foregroundStyle(.white)
+            .background(.darkBackground)
+            .preferredColorScheme(.dark  )
         }
-//        Button("Decode JSON"){
-//            let input = """
-//            {
-//                "name": "Kurt Higa",
-//                "address": {  
-//                    "street": "123 st",
-//                    "country": "USA"
-//                }
-//            }
-//            """
-//            let data = Data(input.utf8)
-//            let decoder = JSONDecoder()
-//            if let user = try? decoder.decode(User.self, from: data){
-//                print(user.address.street)
-//            }
-//        }
     }
 }
 
